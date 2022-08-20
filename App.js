52c61ceb8ddbec8ba6989498ff8a4f7afb42034d
SM\shawn.dong <shawn.dong@seeingmachines.com>
2022-08-20 13:11:32 +1000

    Revert "added maps"
    This reverts commit 3d227ce53fc2cc945c20a302e3e7045c26450429.

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Dimensions } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
-import MapView from "react-native-maps";
 
 // You can import from local files
 import AssetExample from './components/AssetExample';
@@ -12,13 +11,12 @@ import { Card } from 'react-native-paper';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>Bindii App</Text>
+      <Text style={styles.paragraph}>
+        Bindii App
+      </Text>
       <Card>
         <AssetExample />
       </Card>
-      <View style={styles.container}>
-        <MapView style={styles.map} />
-      </View>
     </View>
   );
 }
@@ -26,19 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1",
+    backgroundColor: '#ecf0f1',
     padding: 8,
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
