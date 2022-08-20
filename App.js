6f945d4b930919271dda6ce9cae83fc853c7b0f2
dsknight <dsknight@live.com.au>
2022-08-20 11:54:00 +1000

    Testing commit

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
-        Bindii Bindii Bindii Bindii
+        Bindii App
       </Text>
       <Card>
         <AssetExample />
