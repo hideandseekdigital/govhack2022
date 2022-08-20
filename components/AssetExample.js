d00fbcafc7d2d617cb4fcdf4e60aa764577bf0be
SM\shawn.dong <shawn.dong@seeingmachines.com>
2022-08-20 13:35:59 +1000

    add basic api

--- components/AssetExample.js
+++ components/AssetExample.js
@@ -1,32 +1,79 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
 export default function AssetExample() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch("https://reactnative.dev/movies.json");
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Local files and assets can be imported by dragging and dropping them into the editor
-      </Text>
-      <Image style={styles.logo} source={require('../assets/hideandseek.svg')} />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Local files and assets can be imported by dragging and dropping them
+          into the editor
+        </Text>
+        <Image
+          style={styles.logo}
+          source={require("../assets/hideandseek.png")}
+        />
+      </View>
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>
+                {item.title}, {item.releaseYear}
+              </Text>
+            )}
+          />
+        )}
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 24,
   },
   paragraph: {
     margin: 24,
     marginTop: 0,
     fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   logo: {
     height: 128,
     width: 128,
-  }
+  },
 });
