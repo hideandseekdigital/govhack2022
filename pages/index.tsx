5d927c6db30b16520312eb1bdc514ab525de42ab
SM\shawn.dong <shawn.dong@seeingmachines.com>
2022-08-20 22:52:10 +1000

    full screen maps

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
         <iframe
           src="https://my.atlistmaps.com/map/63473e89-4241-42d7-aad4-415857cd5b45?share=true"
           allow="geolocation"
           width="100%"
-          height="400px"
+          height="800px"
           frameborder="0"
           scrolling="no"
           allowfullscreen
